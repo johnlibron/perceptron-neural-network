63676249f950a93fca1e7280d26b28319634f04e
John James Libron <johnjames.libron@gmail.com>
2022-07-01 14:01:58 +0800

    Initial commit

---
+++ Perceptron/Form.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Perceptron
+{
+    partial class FormPerceptron
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.btnTrain = new System.Windows.Forms.Button();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtBoxResult = new System.Windows.Forms.TextBox();
+            this.cboBoxLetters = new System.Windows.Forms.ComboBox();
+            this.radioBtnCharacterType = new System.Windows.Forms.RadioButton();
+            this.radioBtnCharacter = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.BackColor = System.Drawing.SystemColors.Control;
+            this.tableLayoutPanel.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel.ColumnCount = 5;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel.Location = new System.Drawing.Point(38, 35);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 7;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(173, 218);
+            this.tableLayoutPanel.TabIndex = 0;
+            this.tableLayoutPanel.CellPaint += new System.Windows.Forms.TableLayoutCellPaintEventHandler(this.TableLayoutPanel_CellPaint);
+            this.tableLayoutPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TableLayoutPanel_MouseClick);
+            // 
+            // btnTrain
+            // 
+            this.btnTrain.Location = new System.Drawing.Point(235, 220);
+            this.btnTrain.Margin = new System.Windows.Forms.Padding(2);
+            this.btnTrain.Name = "btnTrain";
+            this.btnTrain.Size = new System.Drawing.Size(84, 33);
+            this.btnTrain.TabIndex = 2;
+            this.btnTrain.Text = "Train";
+            this.btnTrain.UseVisualStyleBackColor = true;
+            this.btnTrain.Click += new System.EventHandler(this.BtnTrain_Click);
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(235, 35);
+            this.btnResult.Margin = new System.Windows.Forms.Padding(2);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(84, 33);
+            this.btnResult.TabIndex = 3;
+            this.btnResult.Text = "Result";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.BtnResult_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(235, 82);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(84, 33);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // txtBoxResult
+            // 
+            this.txtBoxResult.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxResult.Location = new System.Drawing.Point(350, 35);
+            this.txtBoxResult.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBoxResult.Name = "txtBoxResult";
+            this.txtBoxResult.ReadOnly = true;
+            this.txtBoxResult.Size = new System.Drawing.Size(197, 34);
+            this.txtBoxResult.TabIndex = 5;
+            this.txtBoxResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cboBoxLetters
+            // 
+            this.cboBoxLetters.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboBoxLetters.FormattingEnabled = true;
+            this.cboBoxLetters.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z"});
+            this.cboBoxLetters.Location = new System.Drawing.Point(350, 82);
+            this.cboBoxLetters.Margin = new System.Windows.Forms.Padding(2);
+            this.cboBoxLetters.Name = "cboBoxLetters";
+            this.cboBoxLetters.Size = new System.Drawing.Size(67, 24);
+            this.cboBoxLetters.TabIndex = 6;
+            this.cboBoxLetters.SelectedIndexChanged += new System.EventHandler(this.CboBoxLetters_SelectedIndexChanged);
+            // 
+            // radioBtnCharacterType
+            // 
+            this.radioBtnCharacterType.AutoSize = true;
+            this.radioBtnCharacterType.Checked = true;
+            this.radioBtnCharacterType.Location = new System.Drawing.Point(428, 82);
+            this.radioBtnCharacterType.Margin = new System.Windows.Forms.Padding(2);
+            this.radioBtnCharacterType.Name = "radioBtnCharacterType";
+            this.radioBtnCharacterType.Size = new System.Drawing.Size(127, 21);
+            this.radioBtnCharacterType.TabIndex = 8;
+            this.radioBtnCharacterType.TabStop = true;
+            this.radioBtnCharacterType.Text = "Character Type";
+            this.radioBtnCharacterType.UseVisualStyleBackColor = true;
+            this.radioBtnCharacterType.Click += new System.EventHandler(this.RadioBtnCharacterType_Click);
+            // 
+            // radioBtnCharacter
+            // 
+            this.radioBtnCharacter.AutoSize = true;
+            this.radioBtnCharacter.Location = new System.Drawing.Point(428, 107);
+            this.radioBtnCharacter.Margin = new System.Windows.Forms.Padding(2);
+            this.radioBtnCharacter.Name = "radioBtnCharacter";
+            this.radioBtnCharacter.Size = new System.Drawing.Size(91, 21);
+            this.radioBtnCharacter.TabIndex = 9;
+            this.radioBtnCharacter.Text = "Character";
+            this.radioBtnCharacter.UseVisualStyleBackColor = true;
+            this.radioBtnCharacter.Click += new System.EventHandler(this.RadioBtnCharacter_Click);
+            // 
+            // FormPerceptron
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(583, 302);
+            this.Controls.Add(this.radioBtnCharacter);
+            this.Controls.Add(this.radioBtnCharacterType);
+            this.Controls.Add(this.cboBoxLetters);
+            this.Controls.Add(this.txtBoxResult);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.btnTrain);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormPerceptron";
+            this.Text = "Perceptron";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.Button btnTrain;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtBoxResult;
+        private System.Windows.Forms.ComboBox cboBoxLetters;
+        private System.Windows.Forms.RadioButton radioBtnCharacterType;
+        private System.Windows.Forms.RadioButton radioBtnCharacter;
+    }
+}
+
---
+++ Perceptron/Form.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Perceptron
+{
+    public partial class FormPerceptron : Form
+    {
+        private Color[,] _backgroundColors;
+        private int[,] _tableInputs;
+        private int[,] _inputs;
+        private double[] _weights;
+        private double[] _weights1;
+        private double[] _weights2;
+        private double[] _weights3;
+        private double[] _weights4;
+        private double[] _weights5;
+        private int[] _desiredOutputs;
+        private int[] _desiredOutputs1;
+        private int[] _desiredOutputs2;
+        private int[] _desiredOutputs3;
+        private int[] _desiredOutputs4;
+        private int[] _desiredOutputs5;
+        private int _totalRows;
+        private int _totalColumns;
+        private int _totalLetters;
+        private int _totalInputs;
+        private double _learningRate;
+        private int _maxEpoch;
+        private String _letters;
+
+        public FormPerceptron()
+        {
+            InitializeComponent();
+            _totalLetters = 26;
+            _learningRate = 0.1;
+            _maxEpoch = 100;
+            _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            _totalRows = tableLayoutPanel.RowCount;
+            _totalColumns = tableLayoutPanel.ColumnCount;
+            _totalInputs = _totalRows * _totalColumns;
+            _inputs = new int[_totalLetters, _totalInputs + 1]; // add bias
+            _tableInputs = new int[_totalRows, _totalColumns];
+            _backgroundColors = new Color[_totalRows, _totalColumns];
+            _desiredOutputs = new int[_totalLetters];
+            _desiredOutputs1 = new int[_totalLetters];
+            _desiredOutputs2 = new int[_totalLetters];
+            _desiredOutputs3 = new int[_totalLetters];
+            _desiredOutputs4 = new int[_totalLetters];
+            _desiredOutputs5 = new int[_totalLetters];
+            _weights = new double[_totalInputs + 1]; // add bias
+            _weights1 = new double[_totalInputs + 1]; // add bias
+            _weights2 = new double[_totalInputs + 1]; // add bias
+            _weights3 = new double[_totalInputs + 1]; // add bias
+            _weights4 = new double[_totalInputs + 1]; // add bias
+            _weights5 = new double[_totalInputs + 1]; // add bias
+
+            ClearTableLayoutPanel();
+            LoadLetters();
+            LoadDesiredOutputCharacters();
+            LoadWeights();
+        }
+
+        private void TableLayoutPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            int row = 0;
+            int verticalOffset = 0;
+            foreach (int h in tableLayoutPanel.GetRowHeights())
+            {
+                int column = 0;
+                int horizontalOffset = 0;
+                foreach (int w in tableLayoutPanel.GetColumnWidths())
+                {
+                    Rectangle rectangle = new Rectangle(horizontalOffset, verticalOffset, w, h);
+                    if (rectangle.Contains(e.Location))
+                    {
+                        if (_tableInputs[row, column] == 0)
+                        {
+                            _tableInputs[row, column] = 1;
+                            _backgroundColors[row, column] = Color.Black;
+                        }
+                        else
+                        {
+                            _tableInputs[row, column] = 0;
+                            _backgroundColors[row, column] = Color.White;
+                        }
+                        tableLayoutPanel.Invalidate();
+                        tableLayoutPanel.Update();
+                        tableLayoutPanel.Refresh();
+                        return;
+                    }
+                    horizontalOffset += w;
+                    column++;
+                }
+                verticalOffset += h;
+                row++;
+            }
+        }
+
+        private void TableLayoutPanel_CellPaint(object sender, TableLayoutCellPaintEventArgs e)
+        {
+            using (var solidBrush = new SolidBrush(_backgroundColors[e.Row, e.Column]))
+            {
+                e.Graphics.FillRectangle(solidBrush, e.CellBounds);
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtBoxResult.Text = "";
+            cboBoxLetters.SelectedIndex = -1;
+            ClearTableLayoutPanel();
+            tableLayoutPanel.Invalidate();
+            tableLayoutPanel.Update();
+            tableLayoutPanel.Refresh();
+        }
+
+        private void CboBoxLetters_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int count = 0;
+            int index = cboBoxLetters.SelectedIndex;
+            if (index >= 0)
+            {
+                for (int row = 0; row < _totalRows; row++)
+                {
+                    for (int column = 0; column < _totalColumns; column++)
+                    {
+                        if (_inputs[index, count] == 1)
+                        {
+                            _backgroundColors[row, column] = Color.Black;
+                        }
+                        else
+                        {
+                            _backgroundColors[row, column] = Color.White;
+                        }
+                        _tableInputs[row, column] = _inputs[index, count];
+                        count++;
+                    }
+                }
+                tableLayoutPanel.Invalidate();
+                tableLayoutPanel.Update();
+                tableLayoutPanel.Refresh();
+            }
+        }
+
+        private void BtnTrain_Click(object sender, EventArgs e)
+        {
+            if (radioBtnCharacterType.Checked)
+            {
+                Train(0);
+            }
+            else if (radioBtnCharacter.Checked)
+            {
+                Train(1);
+                Train(2);
+                Train(3);
+                Train(4);
+                Train(5);
+            }
+        }
+
+        private void Train(int counter)
+        {
+            int epoch = 0;
+            int totalError = 0;
+            do
+            {
+                totalError = 0;
+                for (int i = 0; i < _totalLetters; i++)
+                {
+                    int output = 0;
+                    int error = 0;
+
+                    switch (counter)
+                    {
+                        case 0:
+                            output = CalculateOutput(_weights, i);
+                            error = _desiredOutputs[i] - output;
+                            break;
+                        case 1:
+                            output = CalculateOutput(_weights1, i);
+                            error = _desiredOutputs1[i] - output;
+                            break;
+                        case 2:
+                            output = CalculateOutput(_weights2, i);
+                            error = _desiredOutputs2[i] - output;
+                            break;
+                        case 3:
+                            output = CalculateOutput(_weights3, i);
+                            error = _desiredOutputs3[i] - output;
+                            break;
+                        case 4:
+                            output = CalculateOutput(_weights4, i);
+                            error = _desiredOutputs4[i] - output;
+                            break;
+                        case 5:
+                            output = CalculateOutput(_weights5, i);
+                            error = _desiredOutputs5[i] - output;
+                            break;
+                    }
+
+                    for (int j = 0; j < _weights.Length; j++)
+                    {
+                        switch (counter)
+                        {
+                            case 0:
+                                _weights[j] += _learningRate * error * _inputs[i, j];
+                                break;
+                            case 1:
+                                _weights1[j] += _learningRate * error * _inputs[i, j];
+                                break;
+                            case 2:
+                                _weights2[j] += _learningRate * error * _inputs[i, j];
+                                break;
+                            case 3:
+                                _weights3[j] += _learningRate * error * _inputs[i, j];
+                                break;
+                            case 4:
+                                _weights4[j] += _learningRate * error * _inputs[i, j];
+                                break;
+                            case 5:
+                                _weights5[j] += _learningRate * error * _inputs[i, j];
+                                break;
+                        }
+                    }
+
+                    totalError += Math.Abs(error);
+                }
+            } while (totalError > 0 && epoch++ < _maxEpoch);
+        }
+
+        private void BtnResult_Click(object sender, EventArgs e)
+        {
+            if (radioBtnCharacterType.Checked)
+            {
+                if (GetResult(_weights) == 0)
+                {
+                    txtBoxResult.Text = "Vowel";
+                }
+                else
+                {
+                    txtBoxResult.Text = "Consonant";
+                }
+            }
+            else if (radioBtnCharacter.Checked)
+            {
+                int index = GetCharacterIndex();
+                if (index >= 0 && index < _totalLetters)
+                {
+                    txtBoxResult.Text = _letters[index].ToString();
+                }
+                else
+                {
+                    txtBoxResult.Text = "Unknown";
+                }
+            }
+        }
+
+        private int GetResult(double[] weights)
+        {
+            double result = 0;
+            int count = 0;
+            for (int row = 0; row < _totalRows; row++)
+            {
+                for (int column = 0; column < _totalColumns; column++)
+                {
+                    result += _tableInputs[row, column] * weights[count++];
+                }
+            }
+            result += 1 * weights[count]; // add bias
+            return (result >= 0) ? 1 : 0;
+        }
+
+        private int GetCharacterIndex()
+        {
+            String binary = "";
+            binary += GetResult(_weights1);
+            binary += GetResult(_weights2);
+            binary += GetResult(_weights3);
+            binary += GetResult(_weights4);
+            binary += GetResult(_weights5);
+            return Convert.ToInt32(binary, 2);
+        }
+
+        private void LoadLetters()
+        {
+            StreamReader sr = new StreamReader(Environment.CurrentDirectory + "//letters.txt");
+            int countRow = 0;
+            int countInput = 0;
+            int countLetter = 0;
+            string line = null;
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (countRow < _totalRows)
+                {
+                    char[] charArray = line.ToCharArray();
+                    foreach (char ch in charArray)
+                    {
+                        _inputs[countLetter, countInput++] = Int32.Parse(ch.ToString());
+                    }
+                    countRow++;
+                }
+                else
+                {
+                    _inputs[countLetter, countInput++] = 1;
+                    int desiredOutput = Int32.Parse(line);
+                    _desiredOutputs[countLetter++] = desiredOutput;
+                    countInput = 0;
+                    countRow = 0;
+                }
+            }
+            sr.Close();
+        }
+
+        private int CalculateOutput(double[] weights, int counter)
+        {
+            double output = 0;
+            for (int i = 0; i < weights.Length; i++)
+            {
+                output += _inputs[counter, i] * weights[i];
+            }
+            return (output >= 0) ? 1 : 0;
+        }
+
+        private void ClearTableLayoutPanel()
+        {
+            for (int row = 0; row < _totalRows; row++)
+            {
+                for (int column = 0; column < _totalColumns; column++)
+                {
+                    _tableInputs[row, column] = 0;
+                    _backgroundColors[row, column] = Color.White;
+                }
+            }
+        }
+
+        private void LoadWeights()
+        {
+            Random random = new Random();
+            for (int i = 0; i < _weights.Length; i++)
+            {
+                _weights[i] = random.NextDouble();
+                _weights1[i] = random.NextDouble();
+                _weights2[i] = random.NextDouble();
+                _weights3[i] = random.NextDouble();
+                _weights4[i] = random.NextDouble();
+                _weights5[i] = random.NextDouble();
+            }
+        }
+
+        private void LoadDesiredOutputCharacters()
+        {
+            StreamReader sr = new StreamReader(Environment.CurrentDirectory + "//desiredOutputCharacters.txt");
+            string line = null;
+            int count = 0;
+            while ((line = sr.ReadLine()) != null)
+            {
+                char[] charArray = line.ToCharArray();
+                _desiredOutputs1[count] = Int32.Parse(charArray[0].ToString());
+                _desiredOutputs2[count] = Int32.Parse(charArray[1].ToString());
+                _desiredOutputs3[count] = Int32.Parse(charArray[2].ToString());
+                _desiredOutputs4[count] = Int32.Parse(charArray[3].ToString());
+                _desiredOutputs5[count] = Int32.Parse(charArray[4].ToString());
+                count++;
+            }
+            sr.Close();
+        }
+
+        private void RadioBtnCharacterType_Click(object sender, EventArgs e)
+        {
+            radioBtnCharacterType.Checked = true;
+            radioBtnCharacter.Checked = false;
+        }
+
+        private void RadioBtnCharacter_Click(object sender, EventArgs e)
+        {
+            radioBtnCharacterType.Checked = false;
+            radioBtnCharacter.Checked = true;
+        }
+    }
+}
